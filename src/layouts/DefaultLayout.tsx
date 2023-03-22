954f16b2b093c73f428d4c9d8c4dcbfb43b197b4
Pietro Weg <pietro.sera@compasso.com.br>
2023-03-21 22:56:56 -0300

    :sparkles: feat: organizando rotas

---
+++ src/layouts/DefaultLayout.tsx
@@ -0,0 +1,21 @@
+import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+import { Header } from "../Components/Header/Header";
+
+export function DefaultLayout() {
+  return (
+    <LayoutContainer>
+      <Header />
+      <Outlet />
+    </LayoutContainer>
+  )
+}
+
+const LayoutContainer = styled.body`
+  max-width: 74rem;
+  margin: 0 auto;
+  padding: 2rem;
+
+  display: flex;
+  flex-direction: column;
+`
---
+++ src/pages/Success/Success.tsx
@@ -0,0 +1,5 @@
+export function Success() {
+  return (
+    <div>Success</div>
+  )
+}
